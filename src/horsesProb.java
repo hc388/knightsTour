203a8b1406a99d249140397a4588e8146ff9f95b
Hrithik <hc388@njit.edu>
2019-11-20 12:51:48 -0500

    final project file added

--- src/horsesProb.java
+++ src/horsesProb.java
@@ -1,162 +1,84 @@
 import java.util.Random;
 
 public class horsesProb {
-    public static int n = 0;
-    public static int[][] dir = new int[][]{{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
-    public static Random rand = new Random();
-
-
-    public static void main(String[] args) {
-        int[][] board = {{0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0}};
-
-
-        int[][] board2 = {{0, -4, 0, -1, 0},
-                {-5, 0, 0, 0, 1},
-                {0, 0, 10, 0, 0},
-                {-1, 0, 0, 0, 3},
-                {0, 1, 0, 3, 0}};
-
-        int x = 3;
-        int y = 2;
-
-        //realMain(board, 2, 2);
-
-        if(secMain(board,x,y)){
-            for(int i = 0; i<5;i++){
-                for(int j = 0; j<5; j++) {
-                    System.out.print(board[i][j]+" ");
-                }
-                System.out.println("\n");
-            }
-        }
-        else {
-            System.out.println("Solution not found.");
-            for(int i = 0; i<5;i++){
-                for(int j = 0; j<5; j++) {
-                    System.out.print(board[i][j]+" ");
-                }
-                System.out.println("\n");
-            }
-        }
-        }
-
-
-
-
-        //if realmain returns true, print the board. if it returns false, then print no moves found.
-
-    public static boolean tempMain(int[][] board, int x, int y){
-
-        if(n >23){
-            return true;
-        }
-        else{
-            int r = rand.nextInt(8);
-            int i = x + dir[r][0];
-            int j = y + dir[r][1];
-
-            if (gameOver(board, i, j))
-                return secMain(board,x,y);
-
-            if (isSafe(board, i, j)) {
-                board[i][j] = 1;
-                n++;
-                return secMain(board,i,j);
-
-            }
-        }
-        return false;
+    public static int[][] dir = new int[][]{{-1, -2}, {-2, 1}, {-2, -1}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+    public static void main(String args[]) {
+        kindaMain();
     }
+    public static boolean kindaMain() {
+        int[][] board = {{-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1}};
 
-
-    public static boolean isSafe(int[][] board, int i, int j) {
-        if (i < 5 && j < 5 && i>=0 && j>= 0) {
-            if (board[i][j] == 0)
-                return true;
-        }
-        return false;
-    }
-
-    public static boolean gameOver(int[][] board, int i, int j) {
-        if (
-                isSafe(board,i-2,j+1)||
-                isSafe(board,i-1,j+2)||
-                isSafe(board,i+1,j+1)||
-                isSafe(board,i+2,j+1)||
-                isSafe(board,i+2,j-1)||
-                isSafe(board,i+1,j-2)||
-                isSafe(board,i-1,j-2)||
-                isSafe(board,i-2,j-1)
-        )
+        if (!secMain(0, 0, 1, board, dir)) {
+            System.out.println("Solution does not exist");
             return false;
+        } else
+            printboard(board);
+
         return true;
     }
 
-
-    public static boolean secMain(int[][] board, int x, int y) {
-
-        if (n == 23) {
+    public static boolean secMain(int x, int y, int movei,
+                                  int board[][], int[][] dir) {
+        Random rand = new Random();
+        int k, i, j;
+        if (movei > 64)
             return true;
-        } else {
-
-
-
+        for (k = 0; k < 8; k++) {
+            int[] tab = new int[8];
             int r = rand.nextInt(8);
-            int i = x + dir[r][0];
-            int j = y + dir[r][1];
-
-            if (gameOver(board, i, j))
-                return secMain(board,x,y);
-
-            if (isSafe(board, i, j)) {
-                board[i][j] = 1;
-                n++;
-                return secMain(board,i,j);
-
-            }
-            else{
-                //return secMain(board,x,y);
-                return false;
-            }
-        }
-    }
-
-
-    public static boolean realMain(int[][] board, int x, int y) {
-
-            //System.out.println(dir[2][0]);
-            //System.out.println(dir[2][1]);
-
-            //System.out.println(board[x][y]);
-        /*for(int i = 0; i<30;i++){
-            int r = rand.nextInt(8);
-            //System.out.print(r);
-
-            System.out.println(board[x+dir[r][0]][y+dir[r][1]]);
-        }*/
-            //System.out.println(board[x+dir[2][0]][y+dir[2][1]]);
-            //System.out.println(board[x+dir[2][0]][y+dir[2][1]]);
-
-
-
-
-            int count = 0;
-            if (isSafe(board, x, y)) {
-                board[x][y] = 1;
-                n++;
-                //return realMain(board, i, j);
-                return true;
-
-
+            if (!contains(tab, r)) {
+                tab[tab.length] = r;
+                i = x + dir[r][0];
+                j = y + dir[r][1];
+                if (isSafe(board, i, j)) {
+                    board[i][j] = movei;
+                    if (secMain(i, j, movei + 1,
+                            board, dir))
+                        return true;
+                    else
+                        board[i][j] = -1;// backtracking
+                }
             }
             else
-                return false;
-                //return realMain(board,x,y);
+                continue;
+        }
 
+        return false;
+    }
+    public static boolean isSafe(int[][] board, int i, int j) {
+        if (i < 8 && j < 8 && i >= 0 && j >= 0) {
+            if (board[i][j] == -1)
+                return true;
+        }
+        return false;
+    }
+    public static void printboard(int[][] board) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("\n");
         }
     }
+    public static boolean contains(final int[] array, final int v) {
 
+        boolean result = false;
+
+        for(int i : array){
+            if(i == v){
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
--- src/scrap.java
+++ src/scrap.java
@@ -2,78 +2,126 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class scrap {
-    public static int[][] dir = new int[][]{{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+    public static int[][] dir = new int[][]{{-1, -2}, {-2, 1}, {-2, -1}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+    public static int[] xcoor = new int[20000];
+    public static int[] ycoor = new int[20000];
     public static Random rand = new Random();
     public static int n = 0;
+    public static int count = 0;
+    public static int tempi = 0;
+    public static int tempj = 0;
+
     public static void main(String[] args) {
-        int[][] board = {{0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0}};
-        int i = 3;
-        int j = 3;
-        while (n < 24) {
-            if (gameOver(board, i, j)) {
-                break;
-            }
-            int tempi = i;
-            int tempj = j;
+        int[][] board = {{0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}};
+        int i = 0;
+        int j = 0;
+        if (kindaMain(board, i, j)) {
+            printboard(board);
+        } else
+            System.out.println("Solution not found.");
 
-            int r = rand.nextInt(8);
-            i += dir[r][0];
-            j += dir[r][1];
-            System.out.println("R is: " + r + " I is: " + i + " j is: " + j);
-            if (isSafe(board, i, j)) {
-                tempi = i;
-                tempj = j;
-                board[i][j] = 1;
-                printboard(board);
-                System.out.println("Next Step:");
-
-            } else {
-                i = tempi;
-                j = tempj;
-                continue;
-            }
-        }
     }
 
+    public static boolean kindaMain(int[][] board, int i, int j) {
+        if (n == 64) {
+            return true;
+        }
+        xcoor[count] = i;
+        ycoor[count] = j;
+        /*if (gameOver(board, i, j)) {
+            n -= 2;
+            board[xcoor[count]][ycoor[count]] = 0;
+
+        }
+        */
+        tempi = i;
+        tempj = j;
+
+        //int r = rand.nextInt(8);
+        for(int b = 0; b<8 ; b++){
+        i += dir[b][0];
+        j += dir[b][1];
+        System.out.println("R is: " + b + " I is: " + i + " j is: " + j);
+        if (isSafe(board, i, j)) {
+            count++;
+
+            n++;
+            tempi = i;
+            tempj = j;
+            board[i][j] = n;
+            printboard(board);
+            System.out.println("Next Step:");
+            if(kindaMain(board, i, j))
+                return true;
+            else {
+                n -= 2;
+                board[xcoor[count]][ycoor[count]] = 0;
+
+                //board[i][j] = 0;
+                //return kindaMain(board, tempi, tempj);
+            }
 
 
+        }
+        else{
+            i = tempi;
+            j = tempj;
+        }
+        }
+                return false;
+        }
 
 
 
 
     public static boolean gameOver(int[][] board, int i, int j) {
         if (
-                isSafe(board,i-2,j+1)||
-                        isSafe(board,i-1,j+2)||
-                        isSafe(board,i+1,j+1)||
-                        isSafe(board,i+2,j+1)||
-                        isSafe(board,i+2,j-1)||
-                        isSafe(board,i+1,j-2)||
-                        isSafe(board,i-1,j-2)||
-                        isSafe(board,i-2,j-1)
+                isSafe(board, i - 2, j + 1) ||
+                        isSafe(board, i - 1, j + 2) ||
+                        isSafe(board, i + 1, j + 1) ||
+                        isSafe(board, i + 2, j + 1) ||
+                        isSafe(board, i + 2, j - 1) ||
+                        isSafe(board, i + 1, j - 2) ||
+                        isSafe(board, i - 1, j - 2) ||
+                        isSafe(board, i - 2, j - 1)
         )
             return false;
         return true;
     }
 
     public static boolean isSafe(int[][] board, int i, int j) {
-        if (i < 5 && j < 5 && i>=0 && j>= 0) {
+        if (i < 8 && j < 8 && i >= 0 && j >= 0) {
             if (board[i][j] == 0)
                 return true;
         }
         return false;
     }
 
-    public static void printboard(int[][] board){
-        for(int i =0; i<5;i++){
-            for(int j = 0;j<5;j++){
-                System.out.print(board[i][j]+" ");
+    public static void printboard(int[][] board) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + " ");
             }
             System.out.println("\n");
         }
+    }
+
+    public static boolean containsZero(int[][] board) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if(board[i][j] == 0)
+                    return true;
+                else
+                    continue;
+            }
+        }
+        return false;
     }
 }
